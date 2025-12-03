41b32083aa6792a92b8c9e82d61c8f0bc1272ab1
thomas_g <thomas.gladin.pro@gmail.com>
2025-12-03 16:57:54 +0100

    Progress on making pages

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
-
+import prisma from './prisma.js'
 const app = express()
+
+const db = await prisma.User.findMany()
+console.log(db)
 
 app.use('/assets', express.static('./assets'))
 
@@ -12,6 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/verify_code', (req, res) => {
    res.render('verify_code')
+})
+
+app.get('/register', (req, res) => {
+   res.render('register')
+})
+
+app.get('/forgotten_password', (req, res) => {
+   res.render('forgotten_password')
+})
+
+app.get('/forgotten_password_verify', (req, res) => {
+   res.render('forgotten_password_verify')
+})
+
+app.get('/register_verify', (req, res) => {
+   res.render('register_verify')
+})
+
+app.get('/set_password', (req, res) => {
+   res.render('set_password')
 })
 
 
