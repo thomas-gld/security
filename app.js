14845b4e15b7082bea9ede01518742a7ce29224d
thomas_g <thomas.gladin.pro@gmail.com>
2025-12-08 22:23:37 +0100

    Update views and Mail sending

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import prisma from './prisma.js'
+import bcrypt from 'bcryptjs'
+import nodemailer from 'nodemailer'
+import { sendMail } from './mail.mjs'
+
 const app = express()
 
 const db = await prisma.User.findMany()
 console.log(db)
+
 
 app.use('/assets', express.static('./assets'))
 
@@ -33,10 +38,62 @@ app.get('/register_verify', (req, res) => {
    res.render('register_verify')
 })
 
-app.get('/set_password', (req, res) => {
-   res.render('set_password')
+app.get('/set_name_password', (req, res) => {
+   res.render('set_name_password')
 })
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/check_credentials', async(req, res) => {
+   const code = Math.floor(1000 + Math.random() * 8999);
+   const textMail = `Votre code de verification est : ${code}`
+   const email = req.body.email
+   const password = req.body.password
+
+   const userList = await prisma.User.findMany({
+      where : {
+         email,
+      }
+   })
+
+   if (userList.length === 0) {
+      res.send('<html>Aucun utilisateurs avec cette adresse mail</html>')
+   }
+
+   if (userList[0].password != password) {
+      res.send('<html>Mot de passe incorrect</html>')
+   }
+   
+   if (userList.length > 0 && userList[0].password === password) {
+      await sendMail({from : 'buisson@enseeiht.fr',
+         to : req.body.email.toLowerCase(),
+         subject : 'Verification',
+         text : textMail})  
+
+      res.render('verify_code');}
+}
+)
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/check_register', async(req, res) => {
+   const email = req.body.email.toLowerCase()
+   const userList = await prisma.User.findMany({
+      where : {
+         email
+      }
+   })
+   
+   console.log(userList);
+
+   if (userList.length > 0) {
+      res.send("<html>Email déja utilisé ...")
+   }
+
+   else {
+      res.render('set_name_password')
+   }
+})
 
 
 
