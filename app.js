7bbb831f589ddfa27f6b3530d0595e262a83a1db
thomas_g <thomas.gladin.pro@gmail.com>
2025-12-09 13:44:51 +0100

    Progress on login and register

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import prisma from './prisma.js'
-import bcrypt from 'bcryptjs'
-import nodemailer from 'nodemailer'
 import { sendMail } from './mail.mjs'
+import { generateCode } from './generate_code.mjs'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+
+
 const db = await prisma.User.findMany()
 console.log(db)
+const email2code = {}
 
 
 app.use('/assets', express.static('./assets'))
@@ -15,7 +18,8 @@ app.use('/assets', express.static('./assets'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-   res.render('login')
+   
+   res.render('login', {error_message : ""})
 })
 
 app.get('/verify_code', (req, res) => {
@@ -42,13 +46,20 @@ app.get('/set_name_password', (req, res) => {
    res.render('set_name_password')
 })
 
+app.get('/set_new_password', (req, res) => {
+   res.render('set_new_password')
+})
+
+app.get('/accueil', (req, res) => {
+   res.render('accueil')
+})
+
+// ------ LOGIN PATH --------------------------------------------------
+
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/check_credentials', async(req, res) => {
-   const code = Math.floor(1000 + Math.random() * 8999);
-   const textMail = `Votre code de verification est : ${code}`
-   const email = req.body.email
-   const password = req.body.password
+   const email = req.body.email.toLowerCase()
 
    const userList = await prisma.User.findMany({
       where : {
@@ -57,22 +68,47 @@ app.post('/check_credentials', async(req, res) => {
    })
 
    if (userList.length === 0) {
-      res.send('<html>Aucun utilisateurs avec cette adresse mail</html>')
+      return res.render('login', {error_message : "Aucun utilisateurs avec cette adresse mail"})
    }
 
-   if (userList[0].password != password) {
-      res.send('<html>Mot de passe incorrect</html>')
+   if (userList[0].password != req.body.password) {
+      return res.render('login', {error_message : "Mot de passe incorrect"})
    }
    
-   if (userList.length > 0 && userList[0].password === password) {
+   if (userList.length > 0 && userList[0].password === req.body.password) {
+      const code = generateCode()
       await sendMail({from : 'buisson@enseeiht.fr',
-         to : req.body.email.toLowerCase(),
+         to : email,
          subject : 'Verification',
-         text : textMail})  
+         text : `Votre code de connexion est : ${code}`
+      }) 
 
-      res.render('verify_code');}
+      email2code[email] = code
+      return res.render('login_verify', {email});
+   }
+
+   console.log(email2code)   
 }
 )
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login_verify_code', async(req, res) => {
+   const email = req.body.email
+   const code = req.body.code
+   console.log(email, code)
+   console.log(email2code[email])
+
+   if (email2code[email] === Number(code)) {
+      res.render('accueil')
+   }
+   else {
+      res.send('<html>Code incorrect</html>')
+   }
+})
+
+// ----------- REGISTER PATH -------------------------------------------------------
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -83,16 +119,64 @@ app.post('/check_register', async(req, res) => {
          email
       }
    })
-   
-   console.log(userList);
 
    if (userList.length > 0) {
-      res.send("<html>Email déja utilisé ...")
+      res.send("<html>Email déja utilisé ...</html>")
    }
 
    else {
-      res.render('set_name_password')
+      const code = generateCode()
+      await sendMail({from : 'buisson@enseeiht.fr',
+         to : email,
+         subject : 'Verification',
+         text : `Votre code de création de compte est : ${code}`
+      }) 
+
+      email2code[email] = code
+      res.render('register_verify', {email})
    }
+})
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/register_verify_code', async(req, res) => {
+   const email = req.body.email
+   const code = req.body.code
+   console.log(email, code)
+   console.log(email2code[email])
+
+   if (email2code[email] === Number(code)) {
+      res.render('set_name_password', {email})
+   }
+   else {
+      res.send('<html>Code incorrect</html>')
+   }   
+})
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/register_new_user', async(req, res) => {
+   const userName = req.body.name
+   const password = req.body.password
+   const passwordConfirm = req.body.password_confirm
+
+   if (password != passwordConfirm) {
+      res.send("<html>Les mots de passe ne correspondent pas</html>")
+   }
+
+   else {
+      await prisma.User.create({
+         data: {
+            email: req.body.email,
+            password: password,
+            name: userName
+         }
+  });
+      res.render('accueil')
+   }
+
+   console.log(userName, password, passwordConfirm)
+   
 })
 
 
---
+++ generate_code.mjs
@@ -0,0 +1,3 @@
+export function generateCode() {
+    return (10000 + Math.floor(Math.random()* 89999));
+}
